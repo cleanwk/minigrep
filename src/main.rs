8fcec04c464ac09c13755a63b98f214f44ad1e84
wukai <wuk.jlu@outlook.com>
2022-04-05 11:40:36 +0800

    提供大小写不敏感匹配，err输出到stderr

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use std::error::Error;
-use std::{env, fs, process};
-use minigrep::{Config, search};
-
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config){
-        println!("Application error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
-
-
 }
